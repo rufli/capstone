2ab5e2a9d1950ce15289884b1a19441b15ea5e0c
Ayunda Yecantika Nurliyanti <yesayesi1303@gmail.com>
2025-05-28 15:02:25 +0700

    Hapus navigation drawer

--- src/scripts/pages/app.js
+++ src/scripts/pages/app.js
@@ -3,41 +3,24 @@ import { getActiveRoute } from '../routes/url-parser';
 
 class App {
   #content = null;
-  #drawerButton = null;
-  #navigationDrawer = null;
 
-  constructor({ navigationDrawer, drawerButton, content }) {
+  constructor({ content }) {
     this.#content = content;
-    this.#drawerButton = drawerButton;
-    this.#navigationDrawer = navigationDrawer;
-
-    this._setupDrawer();
-  }
-
-  _setupDrawer() {
-    this.#drawerButton.addEventListener('click', () => {
-      this.#navigationDrawer.classList.toggle('open');
-    });
-
-    document.body.addEventListener('click', (event) => {
-      if (!this.#navigationDrawer.contains(event.target) && !this.#drawerButton.contains(event.target)) {
-        this.#navigationDrawer.classList.remove('open');
-      }
-
-      this.#navigationDrawer.querySelectorAll('a').forEach((link) => {
-        if (link.contains(event.target)) {
-          this.#navigationDrawer.classList.remove('open');
-        }
-      })
-    });
   }
 
   async renderPage() {
     const url = getActiveRoute();
     const page = routes[url];
 
-    this.#content.innerHTML = await page.render();
-    await page.afterRender();
+    if (page) {
+      this.#content.innerHTML = await page.render();
+      if (page.afterRender) {
+        await page.afterRender();
+      }
+    } else {
+      console.error(`Halaman untuk rute '${url}' tidak ditemukan.`);
+      this.#content.innerHTML = '<h1>404 Halaman Tidak Ditemukan</h1><p>Maaf, halaman yang Anda cari tidak ada.</p>';
+    }
   }
 }
 
